bbbd6f0a3a212ac7bc57b2aa420be13123d5b799
edwardUL99 <47575833+edwardUL99@users.noreply.github.com>
2019-02-21 22:09:23 +0000

    Setup (some) local variables

--- Skewer Knight/menu.lua
+++ Skewer Knight/menu.lua
@@ -17,10 +17,10 @@ local playBtn
 
 -- 'onRelease' event listener for playBtn
 local function onPlayBtnRelease()
-	
+
 	-- go to level1.lua scene
-	composer.gotoScene( "level1", "fade", 500 )
-	
+	composer.gotoScene( "game", "fade", 500 )
+
 	return true	-- indicates successful touch
 end
 
@@ -28,7 +28,7 @@ function scene:create( event )
 	local sceneGroup = self.view
 
 	-- Called when the scene's view does not exist.
-	-- 
+	--
 	-- INSERT code here to initialize the scene
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
@@ -36,50 +36,46 @@ function scene:create( event )
 	local background = display.newImageRect( "background.jpg", display.actualContentWidth, display.actualContentHeight )
 	background.anchorX = 0
 	background.anchorY = 0
-	background.x = 0 + display.screenOriginX 
+	background.x = 0 + display.screenOriginX
 	background.y = 0 + display.screenOriginY
-	
+
 	-- create/position logo/title image on upper-half of the screen
-	local titleLogo = display.newImageRect( "logo.png", 264, 42 )
+	local titleLogo = display.newImageRect( "logo.png", 464, 82 )
 	titleLogo.x = display.contentCenterX
 	titleLogo.y = 100
-	
-	-- create a widget button (which will loads level1.lua on release)
-	playBtn = widget.newButton{
-		label="Play Now",
-		labelColor = { default={255}, over={128} },
-		default="button.png",
-		over="button-over.png",
-		width=154, height=40,
-		onRelease = onPlayBtnRelease	-- event listener function
-	}
-	playBtn.x = display.contentCenterX
-	playBtn.y = display.contentHeight - 125
-	
+
+	-- create a widget button (which will load game.lua on release)
+	playBtn = display.newText("Play Now", display.contentCenterX, display.contentHeight - 125, native.systemFont, 55)
+	playBtn:setFillColor(0, 1, 0)
+	--playBtn.x = display.contentCenterX
+	--playBtn.y = display.contentHeight - 125
+
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
 	sceneGroup:insert( titleLogo )
 	sceneGroup:insert( playBtn )
+
+	playBtn:addEventListener("tap", onPlayBtnRelease)
 end
 
 function scene:show( event )
 	local sceneGroup = self.view
 	local phase = event.phase
-	
+
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
-		-- 
+		--
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
-	end	
+	end
 end
 
 function scene:hide( event )
 	local sceneGroup = self.view
 	local phase = event.phase
-	
+
 	if event.phase == "will" then
 		-- Called when the scene is on screen and is about to move off screen
 		--
@@ -87,17 +83,17 @@ function scene:hide( event )
 		-- e.g. stop timers, stop animation, unload sounds, etc.)
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
-	end	
+	end
 end
 
 function scene:destroy( event )
 	local sceneGroup = self.view
-	
+
 	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
+	--
 	-- INSERT code here to cleanup the scene
 	-- e.g. remove display objects, remove touch listeners, save state, etc.
-	
+
 	if playBtn then
 		playBtn:removeSelf()	-- widgets must be manually removed
 		playBtn = nil

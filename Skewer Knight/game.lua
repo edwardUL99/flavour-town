e6841771b4b721051f60e889bbad0d44f1673fa9
AidanFlahive <47955183+AidanFlahive@users.noreply.github.com>
2019-03-09 21:30:19 +0000

    Add files via upload
    Fixed up version of Prototype with all the assets from the other branches. Lets ONLY use the main Prototype from now on

--- Skewer Knight/game.lua
+++ Skewer Knight/game.lua
@@ -1,146 +1,132 @@
-
-local composer = require( "composer" )
-
+----------------------------------------------------------------------------
+--	NOTES
+----------------------------------------------------------------------------
+--I've renamed most of the variables to be more readable, and moved them about
+--	slightly. Check to see their new names before using them.
+-- (I haven't touched the functions except move them.
+--   Haven't touched the Scene Functions yet)
+--
+--If you want someone to check something out, mark it with an " * ".
+--
+--NOTES ON SYNTAX:
+--[1] Name Variables so you tell exactly what it is just by looking.
+--[2] Same with Functions, make their names a VERB that says what it does.
+--[3] Don't leave empty lines inside functions, it makes it harder to read. Corona tutorials says don't clump code together because it makes it harder to read
+--[4] All comments related to it should be inside the function itself,
+--	  NOT above/below it etc, It's convention for function comments to go above just like in Java no?
+----------------------------------------------------------------------------
+--STARTUP / IMPORTS ETC
+----------------------------------------------------------------------------
+local composer = require( "composer" ) --This is very IMPORTANT
 local scene = composer.newScene()
-
 local sheetInfo = require("spritesheet") --Introduces the functions required to grab sprites from sheet
--- -----------------------------------------------------------------------------------
--- Code outside of the scene event functions below will only be executed ONCE unless
--- the scene is removed entirely (not recycled) via "composer.removeScene()"
--- -----------------------------------------------------------------------------------
 local physics = require( "physics" )
 physics.start()
 physics.setGravity(0,0)
-
---Tables and image sheet required for game 
-local foodsTable = {}
-local skewered = {}
-local maxFoodsOnDisplay = 10 --Arbitrary set to 10 for now
-local maxSkewered = 4 --Arbitrary, set to 4 for now
 local imageSheet = graphics.newImageSheet("spritesheet.png", sheetInfo:getSheet())
--- ------------------
-
-
-local gameLoopTimer
-local scrollSpeed = 2 --Speed of background. Arbitrarily set to 2 for now
-
-------------------
+----------------------------------------------------------------------------
+--VARIABLES BELOW
+----------------------------------------------------------------------------
+---UI related variables--
 local health = 3
 local score = 0
-local healthText      --UI-related variables
+local healthText
 local scoreText
+--------------------
+--Basic Game Variables--
 local paused = false
 local died = false
+local gameLoopTimer
+local runtime = 0
 --------------------
-
-
+--Arrays & tables--
+local looseFoodsTable = {}
+local maxLooseFoods = 10
+local onSkewerArray = {}
+local maxOnSkewer = 4
+local foodCombinations = {}
+local amountOfCombos = 5 --Decide later
+--------------------
 --Graphics variables--
-local character
---We want two of the same background to add scrolling effect.
---Does the background remain constant with character/foods moving
---or is it moving?
+local player
 local bg1
-local bg2
+local bg2 --two SCROLLING backgrounds, to make it look like player is moving)
+local foodScrollSpeed = 2--(Add multiple backgrounds of different speeds,
+local bg2ScrollSpeed = 3 --Food moves at the same speed as the first)
 --------------------
-
--------------------
-local motionx = 0
-local motiony = 0	--Character movement variables
-local speed = 2
--------------------
-
---Boundaries variables
-------------------------------------------------
+--Boundaries variables--
 local leftBound = -(display.viewableContentWidth)
 local rightBound = display.actualContentWidth - display.contentWidth
 local topBound = 0
 local bottomBound = display.actualContentHeight
--------------------------------------------------
-
----------------
-local backGroup
-local mainGroup  ---------Providing variables for displayGroups to be used later
-local uiGroup
----------------
-
---Providing a simple function to return to menu
-local function goToMenu()
+--------------------
+--Providing variables for displayGroups to be used later--
+local backLayer
+local mainLayer
+local uiLayer
+--------------------
+------------------- (I don't know if we're still using these, but I saw they were deleted.
+--local motionx = 0     Keeping them here just in case that was by accident) (Aidan)
+--local motiony = 0	--Character movement variables
+--local speed = 2
+-------------------
+----------------------------------------------------------------------------
+--WORKING FUNCTIONS
+----------------------------------------------------------------------------
+local function goToMainMenu()
 	composer.removeScene("game")
 	composer.gotoScene("menu","fade",500)
 end
 
+local function checkBounds()
+	if (player.x > rightBound) then
+		player.x = rightBound - 20
+	elseif (player.x < leftBound) then
+		player.x = leftBound + 20
+	end
+	--
+	if (player.y < topBound) then
+		player.y = topBound + 20
+	elseif (player.y > bottomBound) then
+		player.y = bottomBound - 20
+	end
+end
+
 local function createObjects()
-	--Will provide code to randomly create certain objects
 	local names = {"bread", "broccoli", "burger", "lettuce", "tomato"} --Will be randomly accessed
-	local name = names[math.random(5)]
-	local newItem = display.newImage(mainGroup, imageSheet, sheetInfo:getFrameIndex(name))
+	local name = names[math.random(#names)]
+	local newItem = display.newImage(mainLayer, imageSheet, sheetInfo:getFrameIndex(name))
 	newItem.height = 200
 	newItem.width = 200
 	newItem.myName = name
-	table.insert(foodsTable, newItem)
-	physics.addBody(newItem, "dynamic", {radius=40, bounce=0.0})
-
+	table.insert(looseFoodsTable, newItem)
+	physics.addBody(newItem, "static", {radius=40, bounce=0.0}) --(We don't need the food
+  --																						to hit each other etc, static is good enough)
 	newItem.x = rightBound + 100
 	newItem.y = math.random(bottomBound)
 	newItem:toBack()
 end
 
-local function addScrollableBg()
-	local bgImage = {type="image",filename="background.png"}
-	--Code to add first background image
-	--Code to add second background image
-end
-
---May not need this function if we are using mouse to drag character
---Will have to add boundaries
-local function keyPressed(event)
-	--Code to maybe add back button functionality to go to main menu
-	return true
-end
-
---Will be used when joystick is added
-local function moveSprite(event)
-	character.x = character.x + motionx
-	character.y = character.y + motiony
-end
-
-local function checkBounds()
-	if (character.x > rightBound) then
-		character.x = rightBound - 20
-	elseif (character.x < leftBound) then
-		character.x = leftBound + 20
-	end
-
-	if (character.y < topBound) then
-		character.y = topBound + 20
-	elseif (character.y > bottomBound) then
-		character.y = bottomBound - 20
-	end
-end
-
-local function dragCharacter(event)
-	local character = event.target
+local function dragplayer(event)
+	local player = event.target
 	local phase = event.phase
-	if (paused ~= true) then 
+	if (paused ~= true) then
 		if ("began" == phase) then
-			display.currentStage:setFocus(character)
-			character.touchOffsetX = event.x - character.x
-			character.touchOffsetY = event.y - character.y
+			display.currentStage:setFocus(player)
+			player.touchOffsetX = event.x - player.x
+			player.touchOffsetY = event.y - player.y
 		elseif ("moved" == phase) then
-			character.x = event.x - character.touchOffsetX
-			character.y = event.y - character.touchOffsetY
+			player.x = event.x - player.touchOffsetX
+			player.y = event.y - player.touchOffsetY
 		elseif ("ended" == phase or "cancelled" == phase) then
 			display.currentStage:setFocus(nil)
 		end
 	end
-	
+	--
 	return true
 end
 
-local runtime = 0
-
---Delta time ensures we have smooth scrolling accross different devices
-local function getDeltaTime()
+local function getDeltaTime() --Delta time ensures we have smooth scrolling accross different devices
 	local temp = system.getTimer()
 	local dt = (temp-runtime) / (1000/60)
 	runtime = temp
@@ -148,88 +134,151 @@ local function getDeltaTime()
 end
 
 local function moveObject(event)
-	local dt = getDeltaTime()
+	local dt = getDeltaTime();
 	if (paused ~= true) then
-		for i = #foodsTable, 1, -1 do
-			foodsTable[i].x = foodsTable[i].x - scrollSpeed * dt
-			if (foodsTable[i].x < -(display.actualContentWidth)) then
-				display.remove(foodsTable[i])
-				table.remove(foodsTable, i)
+		for i = #looseFoodsTable, 1, -1 do
+			looseFoodsTable[i].x = looseFoodsTable[i].x - foodScrollSpeed * dt
+			if (looseFoodsTable[i].x < -(display.actualContentWidth)) then
+				display.remove(looseFoodsTable[i])
+				table.remove(looseFoodsTable, i)
 			end
 		end
 	end
 end
 
---May not be needed?
-local function moveBg(dt)
+local function moveBg(dt) --May not be needed?
 	--Code to move background if necessary.
 	--Google how to do scrolling background
 	--http://lomza.totem-soft.com/tutorial-scrollable-background-in-corona-sdk/
 end
 
-local function enterFrame(event)
+local function enterFrame(event) --( * It will be for the moving background. http://lomza.totem-soft.com/tutorial-scrollable-background-in-corona-sdk/)
 	local dt = getDeltaTime()
 	moveBg(dt)
 end
 
-local function pause()
- --Will provide pause function
-end
-
-local function isEqualTable(table1, table2)
- --To be used in checkCombination method to check if the second table is equal to the table of ideal food combination(2nd table must be in same order as first table)
- --table1 could be implemented as a 2D array of all arrays of ideal combinations, we'll see
- if (#table1 == #table2) then
-  for i=1, #table2, 1 do
-   if (table1[i] ~= table2[i]) then
-    return false
-   end
-  end
- else 
-  return false
- end
-		
- return true
-end
-
-local function checkCombination(namesTable)
- --Will check a table with the food combination and return the score 
-end 
-
-local function store(objectsTable)
- --Will store the names of the objects in the table in an array and pass it to che checkCombination function
- local namesTable = {}
- for i = 1, #objectsTable, 1 do
-   namesTable[i] = objectsTable[i].myName
- end
- 
- return checkCombination(namesTable)
+local function isEqualArray(table1, table2)
+	--Since the score value is only stored at end of each combination table, we can ignore it and check the names only
+	if ((#table1-1) == #table2) then
+		for i = 1, (#table1 - 1) do
+			if (table1[i] ~= table2[i]) then
+				return false
+			end
+		end
+		return true
+	end
+	return false
 end
 
 local function updateSkewer()
  --Will provide code to update the food contents on the skewer
-end 
+end
+
+local function createCombinationsTable()
+	for i = 1, amountOfCombos do --Creating 2D array
+		foodCombinations[i] = {}
+	end
+	--The score values for each combination is in the last position i.e #foodCombinations[i] where i is some number 1-5
+	foodCombinations[1] = {"bread", "bread", "bread", "bread", 500}
+	foodCombinations[2] = {"broccoli", "broccoli", "broccoli", "broccoli", 50}
+	foodCombinations[3] = {"burger", "burger", "burger", "burger", 1000}
+	foodCombinations[4] = {"lettuce", "lettuce", "lettuce", "lettuce", 100}
+	foodCombinations[5] = {"tomato", "tomato", "tomato", "tomato", 200}
+end
+
+--debug to test output on console
+local function printTable(table)
+	for i = 1, #table do
+		print(table[i])
+	end
+	print("------------")
+end
+
+local function print2D(twoD)
+	for i = 1, #twoD do
+		printTable(twoD[i])
+	end
+end
+---Will remove later--
+
+local function checkCombination(namesTable)
+	for i = 1, #foodCombinations do
+		if (isEqualArray(foodCombinations[i], namesTable)) then
+			return foodCombinations[i][#foodCombinations[i]]
+		end
+	end
+	return 500
+end
+
+local function keyPressed(event)
+	if (event.phase == "down") then
+		if (event.keyName == "back") then
+			goToMainMenu()
+		end
+	end
+
+	if (event.phase == "up") then
+		return true
+	end
+end
+
+--The return value is the score to be given to the player
+local function store(objectsTable)
+ local namesTable = {}
+ for i = 1, #objectsTable, 1 do
+   namesTable[i] = objectsTable[i].myName
+ end
+ return checkCombination(namesTable)
+end
 
 local function updateText()
- --Will ensure text is always updated 
+ scoreText = "Score: " .. score
+ healthText = "Health: " .. health
+end
+
+local function gameLoop()
+	createObjects()
+	updateText() -- (* We don't need to update the Text on every frame, just when it's changed.
+end			--So the collisions etc should call the updateText method instead of the loop)
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+--INCOMPLETE/BROKEN FUNCTIONS BELOW
+----------------------------------------------------------------------------
+
+local function addScrollableBg()
+	local bgImage = {type="image",filename="background.png"}
+	--Code to add first background image
+	--Code to add second background image
+end
+
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+--EMPTY/UNUSED FUNCTIONS BELOW
+----------------------------------------------------------------------------
+
+
+--local function moveSprite(event)
+----Will be used when joystick is added
+	--player.x = player.x + motionx
+	--player.y = player.y + motiony
+--end
+
+local function pause()
+ --Will provide pause function
 end
 
 local function resume()
 	--Will provide resume function
 end
 
-local function gameLoop()
-	--Will provide the function for spawning objects randomly
-	createObjects()
-end
-
 local function onCollision(event)
 	--Will provide code for collision events
 end
 
--- -----------------------------------------------------------------------------------
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
 -- Scene event functions
--- -----------------------------------------------------------------------------------
+----------------------------------------------------------------------------
 
 -- create()
 function scene:create( event )
@@ -238,32 +287,41 @@ function scene:create( event )
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 	physics.pause()
 
-	backGroup = display.newGroup()
-	sceneGroup:insert(backGroup)
+	backLayer = display.newGroup()
+	sceneGroup:insert(backLayer)
 
-	mainGroup = display.newGroup()
-	sceneGroup:insert(mainGroup)
+	mainLayer = display.newGroup()
+	sceneGroup:insert(mainLayer)
 
-	uiGroup = display.newGroup()
-	sceneGroup:insert(uiGroup)
+	uiLayer = display.newGroup()
+	sceneGroup:insert(uiLayer)
 
-	local background = display.newImageRect(backGroup, "background.jpg", display.actualContentWidth,display.actualContentHeight)
+	local background = display.newImageRect(backLayer, "background.jpg", display.actualContentWidth,display.actualContentHeight)
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
 
-	character = display.newImageRect(mainGroup, "character.png", 400, 300)
-	character.x = display.contentCenterX - 1000
-	character.y = display.contentCenterY
-	character.myName = "character"
+	player = display.newImageRect(mainLayer, "player.png", 400, 300)
+	player.x = display.contentCenterX - 1000
+	player.y = display.contentCenterY
+	physics.addBody(player, "static", {radius = 30, isSensor=true})
+	player.myName = "player"
 
 	--Health is just text for prototype
-	healthText = display.newText(uiGroup, "Health: " .. health, display.contentCenterX - 1000, display.contentCenterY - 500, native.systemFont, 80)
+	healthText = display.newText(uiLayer, "Health: " .. health, display.contentCenterX - 1000, display.contentCenterY - 500, native.systemFont, 80)
 
 	--Score is text for prototype
-	scoreText = display.newText(uiGroup, "Score: " .. score, display.contentCenterX + 1000, display.contentCenterY - 500, native.systemFont, 80)
+	scoreText = display.newText(uiLayer, "Score: " .. score, display.contentCenterX + 1000, display.contentCenterY - 500, native.systemFont, 80)
 
+	createCombinationsTable()
 
-	character:addEventListener("touch", dragCharacter)
+	--Debug to print test output to console, will remove later
+	print2D(foodCombinations)
+
+	local good = {"lettuce", "lettuce", "lettuce", "lettuce"}
+	print(checkCombination(good))
+	--------------------------------------------------------------
+
+	player:addEventListener("touch", dragplayer)
 end
 
 
@@ -301,6 +359,10 @@ function scene:hide( event )
 	elseif ( phase == "did" ) then
 		-- Code here runs immediately after the scene goes entirely off screen
 		physics.pause()
+		Runtime:removeEventListener("collision",onCollision)
+		Runtime:removeEventListener("enterFrame", checkBounds)
+		Runtime:removeEventListener("enterFrame", moveObject)
+		Runtime:removeEventListener("key", keyPressed)
 	end
 end
 
@@ -314,13 +376,13 @@ function scene:destroy( event )
 end
 
 
--- -----------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
 -- Scene event function listeners
--- -----------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
--- -----------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
 
 return scene

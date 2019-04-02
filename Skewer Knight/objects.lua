8343bff6af0c196b6ee9da9b90937556ecb0e670
edwardUL99 <18222021@studentmail.ul.ie>
2019-04-02 17:00:28 +0100

    Fixed multiple objects spawning bug and added collision filter

--- Skewer Knight/objects.lua
+++ Skewer Knight/objects.lua
@@ -17,15 +17,16 @@ objects.foodsIndices =
 
 function objects:createObjects(layer, rightBound, bottomBound, topBound)
 	--local names = {"bread", "broccoli", "burger", "lettuce", "tomato"} --Will be randomly accessed
+    local foodCollisionFilter = {groupIndex = -2}
     local names = {"bacon", "broccoli", "tomato", "sushi", "cheese", "carrot"}
-    local name = "carrot"--names[math.random(#names)]
+    local name =  names[math.random(#names)]
     --I added the getWidth and getHeight methods to the spritesheet.lua file. Better to use newImageRect
     local newItem = display.newImageRect(layer, imageSheet, sheetInfo:getFrameIndex(name), sheetInfo:getWidth(name), sheetInfo:getHeight(name))
     newItem.height = 200
     newItem.width = 200
     newItem.myName = name
-    physics.addBody(newItem, "dynamic") --(*Static and static cant collide with each other)
-    newItem.x = rightBound + 100
+    physics.addBody(newItem, "dynamic", {filter = foodCollisionFilter}) --(*Static and static cant collide with each other)
+    newItem.x = math.random(rightBound + 100, rightBound + 2000)
     newItem.y = math.random(topBound,bottomBound)
 
     return newItem

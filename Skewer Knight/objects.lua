e49151a2c7f281418005cc56925b2a23ef7b9e65
AidanFlahive <47955183+AidanFlahive@users.noreply.github.com>
2019-04-07 19:55:15 +0100

    Update objects.lua

--- Skewer Knight/objects.lua
+++ Skewer Knight/objects.lua
@@ -22,8 +22,8 @@ function objects:createObjects(layer, rightBound, bottomBound, topBound)
     local name = names[math.random(#names)]
     --I added the getWidth and getHeight methods to the spritesheet.lua file. Better to use newImageRect
     local newItem = display.newImageRect(layer, imageSheet, sheetInfo:getFrameIndex(name), sheetInfo:getWidth(name), sheetInfo:getHeight(name))
-    newItem.height = 200
-    newItem.width = 200
+    newItem.height = 250
+    newItem.width = 250
     newItem.myName = name
     physics.addBody(newItem, "dynamic", {filter = foodCollisionFilter}) --(*Static and static cant collide with each other)
     newItem.x = math.random(rightBound + 100, rightBound + 2000)
